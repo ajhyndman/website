9b91b94d05f91a445ee3050a607e676e49951364
Andrew Hyndman <ajhyndman@hotmail.com>
2016-11-27 16:27:12 +1100

    Extract web-machine to npm module

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
+import { Program } from 'web-machine';
 import { assoc } from 'ramda';
 
-import Program from 'program';
 import NewsFeed from 'components/NewsFeed';
 
 const Title = styled.h1`
